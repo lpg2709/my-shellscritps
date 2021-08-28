a99e9b14b7d18f06901d01e6a2408fc057ff29fc
Leonardo Padilha Guarezi <38223894+lpg2709@users.noreply.github.com>
2021-08-27 22:28:41 -0300

    fix the fucking rendering bug

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -4,7 +4,8 @@ PWD=$(pwd)
 DIR=$(echo "${PWD##*/}")
 S_NAME="dev"
 
-tmux new-session -s $S_NAME -n 'vim' -d 'vim'
+tmux -c new-session -s $S_NAME -n 'vim' -d 'vim'
 tmux new-window -t $S_NAME -n 'bash'
 tmux new-window -t $S_NAME -n 'git'
-tmux attach-session -t $S_NAME
+sleep 0.1 # try fix rendering error with NerdTree
+tmux a -t $S_NAME
