425651b1f1487954413bea186ca63b37652b2df2
Leonardo Padilha Guarezi <38223894+lpg2709@users.noreply.github.com>
2021-09-07 14:22:08 -0300

    Update dev-tmux.sh

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -7,5 +7,5 @@ S_NAME="dev"
 tmux -c new-session -s $S_NAME -n 'vim' -d 'vim'
 tmux new-window -t $S_NAME -n 'bash'
 tmux new-window -t $S_NAME -n 'git'
-sleep 0.1 # try fix rendering error with NerdTree
+sleep 0.5 # try fix rendering error with NerdTree
 tmux a -t $S_NAME
