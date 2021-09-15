05ce1149b76defff9120e1d06bbc703fcd3db2c6
lpg2709 <leonardo_guarezi@hotmail.com>
2021-09-14 21:01:25 -0300

    Update dev-tmux

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -2,10 +2,41 @@
 
 PWD=$(pwd)
 DIR=$(echo "${PWD##*/}")
-S_NAME="dev"
+S_NAME=$DIR
 
-tmux -c new-session -s $S_NAME -n 'vim' -d 'vim'
+# Options
+GIT_WINDOW=0
+
+help(){
+cat << EOF
+Usage: tmux-dev.sh [options]
+
+OPTIONS:
+  --help    Show this message.
+  --git     Start with a window for git.
+
+EOF
+}
+
+while (( "$#" )); do
+	if [ "$1" == "--git" ]; then
+		GIT_WINDOW=1
+	fi
+	if [ "$1" == "--help" ]; then
+		help
+		exit
+	fi
+	shift
+done
+
+
+
+tmux -u new-session -s $S_NAME -n 'vim' -d
 tmux new-window -t $S_NAME -n 'bash'
-tmux new-window -t $S_NAME -n 'git'
-sleep 0.5 # try fix rendering error with NerdTree
-tmux a -t $S_NAME
+if [ $GIT_WINDOW -eq 1 ]; then
+	tmux new-window -t $S_NAME -n 'git'
+fi
+
+tmux send-keys -t $S_NAME:vim "vim" Enter C-b
+sleep 0.5
+tmux attach-session -t $S_NAME
